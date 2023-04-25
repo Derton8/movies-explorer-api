6786ae93194f75ed0d3c2a962a722fe48f96d225
Danil Mysenko <danil.mysenko@yandex.ru>
2023-04-25 17:57:05 +0300

    Добавить схему пользователя

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (value) => validator.isEmail(value),
+      message: 'Invalid email',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+}, { versionKey: false });
+
+module.exports = mongoose.model('user', userSchema);
