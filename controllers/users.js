eaaec8388736f774d8db13de31cad1576e3a7570
Danil Mysenko <danil.mysenko@yandex.ru>
2023-05-22 21:02:22 +0300

    Установить cookie sameSite

--- controllers/users.js
+++ controllers/users.js
@@ -87,6 +87,7 @@ module.exports.login = (req, res, next) => {
           .cookie('jwt', token, {
             maxAge: 7 * 24 * 3600000,
             httpOnly: true,
+            sameSite: 'none',
             secure: true,
           })
           .send({ data: user.delPassword() });
